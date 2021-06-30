15347b8b23cf89ec61a0005f0fce93fbe7783a67
wangst <2224125043@qq.com>
2021-06-30 09:34:28 +0800

    添加了各个子页面的占位符，并实现了子页面的开关功能。

---
+++ static/js/index.js
@@ -0,0 +1,31 @@
+"use strict"
+
+function toggle_visible(id){
+    let ele = document.getElementById(id)
+    ele.style.display = "block";
+}
+
+function toggle_invisible(id){
+    let ele = window.parent.document.getElementById(id)
+    ele.style.display = "none";
+}
+
+function toggle_visibility(id){
+    for (let i = 0; i < subpages.length; i++){
+        if (subpages[i] === id){
+            toggle_visible(id)
+        }
+        else{
+            toggle_invisible(subpages[i])
+        }
+    }
+}
+
+const subpages =
+    ['commodity_wrapper',
+    'warehouse_wrapper',
+    'purchase_wrapper', 'refund_wrapper',
+    'stock_wrapper', 'transfer_wrapper'];
+
+
+
